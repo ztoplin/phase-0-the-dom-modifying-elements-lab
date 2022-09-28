31e643e72e4b8ae9190e478ad82cd470e3bf71d0
Aaron <ztoplin@yahoo.com>
2022-09-28 13:48:34 -0400

    commit tree 65dd4ca325f1cdbd8eea56bb7dbfe0bc6ce81369

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const element = document.getElementById("main")
+element.remove();
+
+const newHeader = document.createElement("h1")
+
+newHeader.id = "victory"
+
+newHeader.textContent = "Aaron is the champion."
